149fe561c742cb2e2e33d7a914ecce28b9535fdf
Sophia Bruce <crazsopapilla@gmail.com>
2024-08-28 15:03:25 -0400

    tuesday 28

---
+++ engine.py
@@ -0,0 +1,17 @@
+from typing import Set, Iterable, Any
+
+from tcod.context import Context
+from tcod.console import Console
+
+from actions import EscapeAction, MovementAction
+from entity import Entity
+from input_handlers import EventHandler
+
+
+class Engine:
+    def __init__(self, entities: Set[Entity], event_handler: EventHandler, player: Entity):
+        self.entities = entities
+        self.event_handler = event_handler
+        self.player = player
+        
+    def handle_events(self,
