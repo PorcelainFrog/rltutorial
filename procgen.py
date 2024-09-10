5db7c38456a4cab6affee396af87a716ae5f93ed
Sophia Bruce <crazsopapilla@gmail.com>
2024-09-10 16:08:58 -0400

    halfway through pt 5

--- procgen.py
+++ procgen.py
@@ -41,7 +41,22 @@ class RectangularRoom:
             and self.y2 >= other.y1
         )    
         
+def place_entities(
+    room: RectangularRoom, dungeon: GameMap, maximum_monsters: int,
+) -> None:
+    number_of_monsters = random.randint(0, maximum_monsters)
+    
+    for i in range(number_of_monsters):
+        x = random.randint(room.x1 + 1, room.x2 - 1)
+        y = random.randint(room.y1 + 1, room.y2 - 1)
         
+        if not any(entity.x == x and entity.y == y for entity in dungeon.entities):
+            if random.random() < 0.8:
+                pass  # TODO: Place an Orc here
+            else:
+                pass  # TODO: Place a Troll here
+
+ 
 def tunnel_between(
     start: Tuple[int, int], end: Tuple[int, int]
 ) -> Iterator[Tuple[int, int]]:
@@ -67,10 +82,11 @@ def generate_dungeon(
     room_max_size: int,
     map_width: int,
     map_height: int,
+    max_monsters_per_room: int,
     player: Entity,
 ) -> GameMap:
     """Generate a new dungeon map."""
-    dungeon = GameMap(map_width, map_height)
+    dungeon = GameMap(map_width, map_height, entities=[player])
     
     rooms: List[RectangularRoom] = []
     
@@ -99,6 +115,8 @@ def generate_dungeon(
             # Dig out a tunnel between this room and the previous one.
             for x, y in tunnel_between(rooms[-1].center, new_room.center):
                 dungeon.tiles[x, y] = tile_types.floor
+                
+        place_entities(new_room, dungeon, max_monsters_per_room)
                 
         # Finally, append the new room to the list.
         rooms.append(new_room)
